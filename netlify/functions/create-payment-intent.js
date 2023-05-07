be571c1e9c7615cf34833f92049acabd4cbd36bf
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-05-07 13:10:07 +0300

    added default amount

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -4,9 +4,8 @@ const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
-
     const paymentIntent = await stripe.paymentIntents.create({
-      amount,
+      amount: 100,
       currency: "usd",
       payment_method_types: ["card"],
     });
